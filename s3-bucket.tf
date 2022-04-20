cc6af1a5b4ae81e37fa7a9778c784a2a0c80db31
Wes Belt <wes.belt@ampf.com>
2022-04-20 16:49:16 +0000

    Added s3 bucket module

---
+++ s3-bucket.tf
@@ -0,0 +1,11 @@
+module "s3-bucket" {
+  source  = "app.terraform.io/ampf-wbelt/s3-bucket/aws"
+  version = "2.8.0"
+  bucket = "my-s3-bucket"
+  bucket_prefix = "${var.prefix}"
+  acl    = "private"
+
+  versioning = {
+    enabled = true
+  }
+}
